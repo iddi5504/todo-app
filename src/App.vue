45e8ce9baec80524a75e327c9c0f7e8bac349b66
Iddi <Iddi5504@gmail.com>
2022-10-22 00:25:00 +0100

    new commit

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,116 @@
 <template>
   <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
-    <router-view/>
+    <input type="text" v-model="username" name="name">
+    <input type="email" v-model="email" name="email">
+    <input type="password" v-model="password" name="email">
+    <button @click="post">Submit</button>
+    <button @click="signUp">Sign up</button>
+    <button @click="signOut">Sign out</button>
+    <br>
+    <input type="text" v-model="id">
+    <button style="background-color:red;" @click="deleteUser">Delete</button>
+    <button style="background-color:green;" @click="getUser">get</button>
+    <input type="text" v-model="editedEmail" placeholder="edit email">
+    <button style="background-color:cyan;" @click="editEmail">Edit</button>
+
   </div>
 </template>
+<script>
+import { users } from './firebase'
+import { getDocs } from "firebase/firestore";
+import { addDoc } from "firebase/firestore";
+import { 
+  getAuth,
+  createUserWithEmailAndPassword,
+  signOut
+} from "firebase/auth";
+import { doc,
+   deleteDoc, 
+   onSnapshot,
+    query,
+   serverTimestamp,
+    orderBy,
+    getDoc,
+    updateDoc
+  } from "firebase/firestore";
+import { database } from "./firebase"
 
+export default {
+  data() {
+    return {
+      username: "",
+      email: "",
+      id: "",
+      editedEmail:"",
+      password:""
+    }
+  },
+  methods: {
+    post() {
+      addDoc(users, {
+         username: this.username,
+          email: this.email,
+          createdAt:serverTimestamp()
+
+         })
+        .then(data => {
+          console.log(data)
+        })
+    },
+    deleteUser() {
+      console.log()
+      let thisUser = doc(database, "users", this.id)
+      deleteDoc(thisUser)
+        .then(data => { data })
+      console.log(thisUser)
+    },
+    getUser() {
+      const user= doc(database, "users", this.id)
+      getDoc(user)
+      .then(user => { console.log(user.data())});
+
+    },
+    editEmail(){
+      const user=doc(database,"users",this.id)
+      updateDoc(user,{
+        email:this.editedEmail
+      })
+    },
+    // creating an account
+    signUp(){
+      const auth=getAuth()
+      createUserWithEmailAndPassword(auth,this.email,this.password)
+      .then(response => { console.log(response)})
+    },
+    signOut(){
+      const auth=getAuth()
+
+      signOut(auth)
+      .then(response=> { console.log(response)})
+      .catch(error => { console.log(error)})
+    }
+  },
+  created() {
+    const Users= query(users,orderBy("createdAt",'desc'))
+    getDocs(Users)
+    .then(snapshot=>{
+      // console.log({...snapshot.data()})
+     let users=[]
+     snapshot.docs.forEach(doc=>{
+      users.push({...doc.data(),id:doc.id})
+     })
+     console.log(users)
+    })
+
+    onSnapshot(users, (snapshot) => {
+    var localUsers = [];
+      snapshot.docs.forEach(doc => {
+        localUsers.push({ ...doc.data(), id: doc.id })
+      })
+      console.log(localUsers)
+    })
+  }}
+</script>
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
@@ -15,6 +118,27 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+}
+
+input {
+  width: 70vw;
+  padding: 10px;
+  font-size: large;
+  margin: 20px;
+  background: aliceblue;
+}
+
+button {
+  padding: 10px;
+  font-size: large;
+  margin: 10px;
+  border-radius: 10px;
+  width: 100px;
 }
 
 nav {
---
+++ src/firebase.js
@@ -0,0 +1,24 @@
+import { initializeApp } from "firebase/app"
+import {collection} from "firebase/firestore";
+import {getFirestore} from 'firebase/firestore';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBuMhYdNDxrBjr5TsyAy02h9Ee1tvQrQ8M",
+    authDomain: "test-1f957.firebaseapp.com",
+    projectId: "test-1f957",
+    storageBucket: "test-1f957.appspot.com",
+    messagingSenderId: "684367844550",
+    appId: "1:684367844550:web:81a53360b0657c65e833c7"
+  };
+
+initializeApp(firebaseConfig);
+
+
+// initializing firestore
+const database= getFirestore()
+// set up collection
+const users=collection(database, "users");
+// collect data from collection
+export {users,database}
+
+
